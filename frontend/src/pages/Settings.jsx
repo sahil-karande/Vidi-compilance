ea2d23387da8f3cd37afc4c89f6baead66110646
Sahil Karande <sahilkarande6@gmail.com>
2026-07-15 23:21:27 +0530

    update settings

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -48,6 +48,7 @@ export default function Settings() {
 
   useEffect(() => {
     if (user) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setProfileForm((prev) => ({
         ...prev,
         name: user.name || prev.name,
